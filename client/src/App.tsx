450bb5c338b199cf4ca3fae2c5686cb43aa38358
Ваня Лысенко <v4nkaxd@gmail.com>
2024-12-13 14:33:20 +0300

    project-init

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="bg-slate-600">hello</div>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Подключение к MongoDB установлено.");
+    app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
